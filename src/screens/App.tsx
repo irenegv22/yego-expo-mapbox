2ac47ffb935e4ca3373cbbebdf2f33bea21dddd4
irenegv22 <irene.gv94@gmail.com>
2024-02-18 20:01:47 +0000

    move colect data to app screen

--- src/screens/App.tsx
+++ src/screens/App.tsx
@@ -1,19 +1,109 @@
-import React from 'react';
-import {SafeAreaView, StyleSheet, View, Text, StatusBar, Button} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {SafeAreaView, StyleSheet, View, Text, StatusBar, Button, ActivityIndicator} from 'react-native';
 import {useNavigation} from '@react-navigation/native';
 import Color from '../../static/types/colors';
+import {Vehicle, VehicleStatus} from '@/types/vehicles';
+import {Paths, get} from 'src/services/https';
+import {useDispatch, useSelector} from 'react-redux';
+import {selectUserLocationMemoized} from 'src/redux/selectors/userLocation';
+import {calculateDistance} from 'src/utils/distance.util';
+import {setVehicles} from 'src/redux/reducers/vehicles';
+import {setUserLocation} from 'src/redux/reducers/userLocation';
+import {systemErrorAlert} from 'src/alerts/systemErrorAlert';
+import {locationPermissionAlert} from 'src/alerts/locationPermissionAlert';
+import {locationService} from 'src/services/locationService';
 
 const App: React.FC = () => {
   const navigation = useNavigation();
+  const dispatch = useDispatch();
+  const userLocation = useSelector(selectUserLocationMemoized);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
 
-  const navigateToMapScreen = () => {
+  useEffect(() => {
+    handleUserLocation();
+  }, []);
+
+  const navigateToMapScreen = async () => {
+    await getVehicles();
     navigation.navigate('MapScreen' as never);
+  };
+
+  const getVehicles = async () => {
+    try {
+      setIsLoading(true);
+      const vehicles: Vehicle[] = await get(Paths.TechnicalTest);
+      if (vehicles as Vehicle[]) {
+        const sortVehicles = addDistanceToVehicles(vehicles);
+        dispatch(setVehicles(sortVehicles));
+      } else {
+        return systemErrorAlert();
+      }
+    } catch (e) {
+      return systemErrorAlert();
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  const addDistanceToVehicles = (vehicles: Vehicle[]) => {
+    const userLat = userLocation.latitude;
+    const userLng = userLocation.longitude;
+
+    const updatedVehicles = vehicles.map(vehicle => {
+      if (vehicle.status === VehicleStatus.AVAILABLE) {
+        const distance = calculateDistance(userLat, userLng, vehicle.lat, vehicle.lng);
+        return {...vehicle, distance};
+      } else {
+        return vehicle;
+      }
+    });
+
+    updatedVehicles.sort((a, b) => {
+      if (a.distance !== undefined && b.distance !== undefined) {
+        return a.distance - b.distance;
+      } else if (a.distance === undefined && b.distance !== undefined) {
+        return 1;
+      } else if (a.distance !== undefined && b.distance === undefined) {
+        return -1;
+      } else {
+        return 0;
+      }
+    });
+    return updatedVehicles;
+  };
+
+  const handleUserLocation = async () => {
+    try {
+      setIsLoading(true);
+      const permission = await locationService.requestPermissions();
+      if (!permission) locationPermissionAlert();
+      const location = await locationService.getLocation();
+      const longitudeLocation = location?.coords.longitude;
+      const latitudLocation = location?.coords.latitude;
+      if (longitudeLocation && latitudLocation) {
+        const actualUserLocation = {
+          latitude: latitudLocation,
+          longitude: longitudeLocation,
+        };
+        dispatch(setUserLocation(actualUserLocation));
+      }
+      return true;
+    } catch (e) {
+      systemErrorAlert();
+    } finally {
+      setIsLoading(false);
+    }
   };
 
   return (
     <>
       <StatusBar barStyle="dark-content" />
       <SafeAreaView>
+        {isLoading && (
+          <View style={styles.loader}>
+            <ActivityIndicator style={{position: 'relative'}} size="large" color={Color.GREEN} />
+          </View>
+        )}
         <View style={styles.body}>
           <View style={styles.sectionContainer}>
             <Text style={styles.sectionTitle}>YEGO - Technical Test</Text>
@@ -41,6 +131,18 @@ const styles = StyleSheet.create({
     fontSize: 24,
     fontWeight: '600',
     color: Color.BLACK,
+  },
+  loader: {
+    ...StyleSheet.absoluteFillObject,
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    height: '100%',
+    width: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    zIndex: 20,
+    backgroundColor: 'rgba(146, 139, 139, 0.5)',
   },
 });
 
--- src/screens/MapView.tsx
+++ src/screens/MapView.tsx
@@ -1,23 +1,15 @@
-import React, {useMemo, useRef, useState, useEffect} from 'react';
+import React, {useMemo, useRef, useState} from 'react';
 import {useSelector} from 'react-redux';
 import {selectVehicles} from '../redux/selectors/vehicles';
 import {View, StyleSheet} from 'react-native';
-import {useDispatch} from 'react-redux';
-import {setVehicles} from 'src/redux/reducers/vehicles';
-import {setUserLocation} from 'src/redux/reducers/userLocation';
 import {Vehicle, VehicleStatus, VehicleStatusIcons} from '@/types/vehicles';
 import MapView, {Marker} from 'react-native-maps';
-import {Paths, get} from 'src/services/https';
-import {systemErrorAlert} from 'src/alerts/systemErrorAlert';
 import BottomSheetComponent from 'src/components/BottomSheet/BottomSheet';
 import BottomSheet from '@gorhom/bottom-sheet';
-import {locationService} from 'src/services/locationService';
-import {locationPermissionAlert} from 'src/alerts/locationPermissionAlert';
 import {selectUserLocationMemoized} from 'src/redux/selectors/userLocation';
 import {calculateDistance} from 'src/utils/distance.util';
 
 const MyComponent = () => {
-  const dispatch = useDispatch();
   const vehicles = useSelector(selectVehicles);
   const userLocation = useSelector(selectUserLocationMemoized);
   const [selectedVehicle, setSelectedVehicle] = useState<Vehicle>(vehicles[0]);
@@ -26,57 +18,11 @@ const MyComponent = () => {
   const bottomSheetRef = useRef<BottomSheet>(null);
   const snapPoints = useMemo(() => ['20%'], []);
 
-  useEffect(() => {
-    handleUserLocation();
-    getVehicles();
-  }, []);
-
   const handleMarkerPress = (vehicle: Vehicle, id: number) => {
     if (vehicle.status === VehicleStatus.AVAILABLE) {
       setSelectedVehicle(vehicle);
       setCurrentIndex(id);
     }
-  };
-
-  const getVehicles = async () => {
-    try {
-      const vehicles: Vehicle[] = await get(Paths.TechnicalTest);
-      if (vehicles as Vehicle[]) {
-        const sortVehicles = addDistanceToVehicles(vehicles);
-        dispatch(setVehicles(sortVehicles));
-      } else {
-        return systemErrorAlert();
-      }
-    } catch (e) {
-      return systemErrorAlert();
-    }
-  };
-
-  const addDistanceToVehicles = (vehicles: Vehicle[]) => {
-    const userLat = userLocation.latitude;
-    const userLng = userLocation.longitude;
-
-    const updatedVehicles = vehicles.map(vehicle => {
-      if (vehicle.status === VehicleStatus.AVAILABLE) {
-        const distance = calculateDistance(userLat, userLng, vehicle.lat, vehicle.lng);
-        return {...vehicle, distance};
-      } else {
-        return vehicle;
-      }
-    });
-
-    updatedVehicles.sort((a, b) => {
-      if (a.distance !== undefined && b.distance !== undefined) {
-        return a.distance - b.distance;
-      } else if (a.distance === undefined && b.distance !== undefined) {
-        return 1;
-      } else if (a.distance !== undefined && b.distance === undefined) {
-        return -1;
-      } else {
-        return 0;
-      }
-    });
-    return updatedVehicles;
   };
 
   const handleVehiclesIcon = (status: VehicleStatus) => {
@@ -85,22 +31,6 @@ const MyComponent = () => {
     }
     const defaultIcon = require('@/images/icon_scooter_green.png');
     return defaultIcon;
-  };
-
-  const handleUserLocation = async () => {
-    const permission = await locationService.requestPermissions();
-    if (!permission) locationPermissionAlert();
-    const location = await locationService.getLocation();
-    const longitudeLocation = location?.coords.longitude;
-    const latitudLocation = location?.coords.latitude;
-    if (longitudeLocation && latitudLocation) {
-      const actualUserLocation = {
-        latitude: latitudLocation,
-        longitude: longitudeLocation,
-      };
-      dispatch(setUserLocation(actualUserLocation));
-    }
-    return;
   };
 
   const handleUserVehicleDistance = (vehicle: Vehicle) => {
